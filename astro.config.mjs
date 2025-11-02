62a6c059bcf945e46aa05a0b581142f5b53b89b6
Freddie-Fu-Git <ms.freddiefu@outlook.com>
2025-11-02 23:35:14 +0800

    feat: 添加 rehype-external-links 支持外部链接

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeComponents from "rehype-components";/* Render the custom directive content */
 import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
+import rehypeExternalLinks from "rehype-external-links";
 import remarkDirective from "remark-directive";/* Handle directives */
 import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
 import remarkMath from "remark-math";
@@ -119,6 +120,13 @@ export default defineConfig({
       rehypePlugins: [
           rehypeKatex,
           rehypeSlug,
+          [
+              rehypeExternalLinks,
+              {
+                  target: "_blank",
+                  rel: ["noopener", "noreferrer"],
+              },
+          ],
           [
               rehypeComponents,
               {
